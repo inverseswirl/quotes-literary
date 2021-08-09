65f3f9f6788045b617bdf54b40c0df10e678eb54
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2021-08-09 11:45:30 +1000

    images

--- src/assets/imagebundle/images.js
+++ src/assets/imagebundle/images.js
@@ -3,17 +3,17 @@ import airgondola from '../img/airgondola.jpeg';
 import bee from    '../img/bee.jpeg';
 import buddha from '../img/buddha.jpeg';
 import clock from  '../img/clock.webp';
-import  child1 from '../img/child1.jpeg';
+
 import  coffee from '../img/coffee.webp';
 import  dog1 from '../img/dog1.jpeg';
 import  dog2 from '../img/dog2.jpeg';
 import  flower1 from '../img/flower1.jpeg';
-import  hydrangea from '../img/hydrangea.webp';
+
 import  japan from '../img/japan.jpeg';
 import   kitten1 from '../img/kitten1.webp';
 import  kualalumpur from '../img/kualalumpur.jpeg';
 import  lake1 from '../img/lake1.webp';
-import  ricefield from '../img/ricefield.jpeg';
+
 import  sydney from '../img/sydney.jpeg';
 import  whiteling from '../img/whiteling.webp';
 import  wolf from '../img/wolf.jpeg';
@@ -24,8 +24,8 @@ import  turtle from '../img/turtle.jpeg';
 
 
 
- export const images= [  airgondola, bee, buddha,clock,child1,coffee,dog1,dog2,flower1,hydrangea,
-japan,kitten1,kualalumpur,lake1,ricefield,sydney,whiteling,wolf,pollination,maltesepup,turtle
+ export const images= [  airgondola, bee, buddha,clock,coffee,dog1,dog2,flower1,
+japan,kitten1,kualalumpur,lake1,sydney,whiteling,wolf,pollination,maltesepup,turtle
 
 ]
 
